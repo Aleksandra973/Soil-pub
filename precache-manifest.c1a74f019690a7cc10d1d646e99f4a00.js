85d4aa3d89412d2cbeb46c4e325fb6089f78c7ae
Sanechka <sanechka@B-36L3MD6M-0355.local>
2020-11-24 17:06:22 +0300

    first commit

---
+++ precache-manifest.c1a74f019690a7cc10d1d646e99f4a00.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1ab1ed2dd09887e83f0",
+    "url": "/css/app.80e23cbc.css"
+  },
+  {
+    "revision": "df1c532efdf388847a25",
+    "url": "/css/chunk-vendors.c5b0137c.css"
+  },
+  {
+    "revision": "376455b7e55266c4c86cd458178de549",
+    "url": "/img/agriculture.svg"
+  },
+  {
+    "revision": "7ae661e08db78826ff60077ceb83a662",
+    "url": "/img/bacteria.svg"
+  },
+  {
+    "revision": "0d12b58af2849df31aefd12df6ff4627",
+    "url": "/img/digging.svg"
+  },
+  {
+    "revision": "a2faa8e2c17399f7f9bc599dc436e5a9",
+    "url": "/img/facgz4.jpg"
+  },
+  {
+    "revision": "494e9f3f0830f287de05d572ced72125",
+    "url": "/img/grass.494e9f3f.jpg"
+  },
+  {
+    "revision": "494e9f3f0830f287de05d572ced72125",
+    "url": "/img/grass.jpg"
+  },
+  {
+    "revision": "9e29b896a778415a3e9257618c2739f7",
+    "url": "/img/history.jpg"
+  },
+  {
+    "revision": "4c19eb60d24e0ad3758109f61279f32d",
+    "url": "/img/line-graph.svg"
+  },
+  {
+    "revision": "6cad33f488cfd53cf99076ca6865a5d3",
+    "url": "/img/logo.png"
+  },
+  {
+    "revision": "0767c64511859482c26cff70ca1f1ff4",
+    "url": "/img/monitoring.svg"
+  },
+  {
+    "revision": "4747c265e7c8d3470a4944f648b35727",
+    "url": "/img/soils.jpg"
+  },
+  {
+    "revision": "e833c32ebe81ca82a76e2a57d4fd8173",
+    "url": "/img/staczim.jpeg"
+  },
+  {
+    "revision": "0d6ef551eb727747a0fbdfc2692d0b75",
+    "url": "/img/water-drop.svg"
+  },
+  {
+    "revision": "0e38d153b50754ef7c6a352700326941",
+    "url": "/img/МГУ._Старые_теплицы_(15917303148).jpg"
+  },
+  {
+    "revision": "ef843805ee966feaf1c38dc819a4f683",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1ab1ed2dd09887e83f0",
+    "url": "/js/app.52c7ea6a.js"
+  },
+  {
+    "revision": "df1c532efdf388847a25",
+    "url": "/js/chunk-vendors.8698ff66.js"
+  },
+  {
+    "revision": "12e9b076e4ed1a17febecbae4510483d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
